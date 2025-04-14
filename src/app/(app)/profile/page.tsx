8bee5615cfe854c7efc73b3bcf572ddda8b672af
Nikita Zabashtin <147318691+NikitaKadekomst@users.noreply.github.com>
2025-04-15 00:20:35 +0300

    💄 Создание маршрута /profile для страницы юзера

---
+++ src/app/(app)/profile/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { ProfilePage } from '@/features/user/ui/ProfilePage/ProfilePage';
+
+const Profile = () => {
+  return (
+    <ProfilePage />
+  )
+}
+
+export default Profile;
