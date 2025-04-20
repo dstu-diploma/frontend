ec8b9d4951cd60dfbeb53b8c535575a127b7c4c3
Nikita Zabashtin <147318691+NikitaKadekomst@users.noreply.github.com>
2025-04-20 13:53:14 +0300

    ✨ API для авторизации и валидации для полей в формах логина и регистрации

--- src/features/user/api/userApi.ts
+++ src/features/user/api/userApi.ts
@@ -1,35 +1,41 @@
-import { User, UserProfile } from '../model/types';
+import axiosInstance from '@/shared/api/axios';
+import { useMutation } from '@tanstack/react-query';
+import { RegisterRequestBody, AuthResponseBody, LoginRequestBody } from '../model/types';
+
+const API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost';
 
 export const userApi = {
-  getProfile: async (): Promise<UserProfile> => {
-    return {
-      name: 'Иван Иванов',
-      email: 'ivan@example.com',
-      bio: 'Frontend разработчик',
-      phone: '+7 (999) 123-45-67',
-      location: 'Ростов-на-Дону',
-    };
-  },
+    registerUser: async (data: RegisterRequestBody): Promise<AuthResponseBody> => {
+        const response = await axiosInstance.post(`${API_URL}/user/`, data);
+        return response.data;
+    },
+    loginUser: async (data: LoginRequestBody): Promise<AuthResponseBody> => {
+        const formData = new URLSearchParams();
+        formData.append('username', data.username);
+        formData.append('password', data.password);
+    
+        const response = await axiosInstance.post(`${API_URL}/user/login`, formData, {
+            headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+            },
+        });
+        return response.data;
+    },
+    refreshToken: async () => {
+        const response = await axiosInstance.post(`${API_URL}/user/refresh`);
+        return response.data;
+    }
+}
 
-  updateProfile: async (profile: Partial<UserProfile>): Promise<UserProfile> => {
-    console.log('Updating profile:', profile);
-    return {
-      name: profile.name || 'Иван Иванов',
-      email: profile.email || 'ivan@example.com',
-      bio: profile.bio || 'Frontend разработчик',
-      phone: profile.phone || '+7 (999) 123-45-67',
-      location: profile.location || 'Ростов-на-Дону',
-      avatar: profile.avatar,
-    };
-  },
-
-  updateAvatar: async (file: File): Promise<string> => {
-    return new Promise((resolve) => {
-      const reader = new FileReader();
-      reader.onloadend = () => {
-        resolve(reader.result as string);
-      };
-      reader.readAsDataURL(file);
-    });
-  },
-}; 
+export const userApiMutations = {
+    register: () => {
+        return useMutation({
+          mutationFn: userApi.registerUser
+        });
+    },
+    login: () => {
+        return useMutation({
+          mutationFn: userApi.loginUser
+        });
+    },
+}
--- src/features/user/model/types.ts
+++ src/features/user/model/types.ts
@@ -1,16 +1,47 @@
 export interface User {
   id: string;
-  name: string;
+  first_name: string;
+  last_name: string;
+  patronymic: string;
+  register_date: string;
   email: string;
-  avatar?: string;
+  about: string | null;
+  birthday: string | null;
   role: 'user' | 'admin';
 }
 
-export interface UserProfile {
-  name: string;
-  email: string;
-  avatar?: string;
-  bio?: string;
-  phone?: string;
-  location?: string;
+export interface UserProfile extends Omit<User, 'id'> {
+  role: 'user' | 'admin'
 } 
+
+export type BaseUserType = {
+  id: number,
+  first_name: string,
+  last_name: string,
+  patronymic: string,
+  register_date: string,
+  email: string,
+  about: string | null,
+  birthday: string | null
+}
+
+export interface RegisterRequestBody {
+  email: string;
+  first_name: string;
+  last_name: string;
+  patronymic: string;
+  password: string;
+}
+
+export interface AuthResponseBody {
+  user: BaseUserType,
+  access_token: string,
+  refresh_token: string
+}
+
+export interface LoginRequestBody {
+  username: string;
+  password: string;
+}
+
+
