8ab6f843e97e8e0ca6fb83a0f95aba49c13adb7f
Nikita Zabashtin <147318691+NikitaKadekomst@users.noreply.github.com>
2025-05-01 00:02:06 +0300

    🐛 Фикс стилей хедера и логики вывода в нем данных

--- src/widgets/Header/ui/Header.tsx
+++ src/widgets/Header/ui/Header.tsx
@@ -3,30 +3,28 @@
 import Link from 'next/link';
 import Image from 'next/image';
 import styles from './Header.module.css';
+import { getAuthCookies } from '@/shared/lib/helpers/cookies';
 
-interface HeaderProps {
-  user: {
-    name: string;
-    avatar?: string;
-  };
-}
-
-export const Header = ({ user }: HeaderProps) => {
+export const Header = () => {
+  const cookies = getAuthCookies();
+  const user = cookies.user;
+  
   return (
     <header className={styles.header}>
       <div className={styles.headerContent}>
         <Link href="/" className={styles.brand}>
           <h1 className={styles.brandTitle}>Packathon</h1>
         </Link>
-
         <div className={styles.userInfo}>
-          <span className={styles.userName}>{user.name}</span>
+          <Link href="/profile" className={styles.userName}>
+            <span>{user?.first_name && user?.last_name && `${user.first_name} ${user.last_name}`}</span>
+          </Link>
           <div className={styles.avatarWrapper}>
-            {user.avatar ? (
+            {user?.avatar ? (
               <Link href="/profile">
                 <Image
                   src={user.avatar}
-                  alt={user.name}
+                  alt={user.first_name}
                   width={40}
                   height={40}
                   className={styles.avatar}
@@ -34,7 +32,7 @@ export const Header = ({ user }: HeaderProps) => {
               </Link>
             ) : (
               <Link href="/profile" className={styles.avatarPlaceholder}>
-                {user.name.charAt(0).toUpperCase()}
+                {user?.first_name.charAt(0).toUpperCase()}
               </Link>
             )}
           </div>
