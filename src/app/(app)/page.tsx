59009e7e42119afc47071e8790bb519a786e1718
Nikita Zabashtin <147318691+NikitaKadekomst@users.noreply.github.com>
2025-04-30 23:55:30 +0300

    ♻️ Смена маршрута главной с /dashboard на /

--- src/app/(app)/page.tsx
+++ src/app/(app)/page.tsx
@@ -1,14 +1,5 @@
 'use client';
 
-import { useEffect } from 'react';
-import { useRouter } from 'next/navigation';
-
 export default function Home() {
-  const router = useRouter();
-
-  useEffect(() => {
-    router.push('/dashboard');
-  }, [router]);
-
-  return null;
+  return <div>Главная страница</div>
 } 
