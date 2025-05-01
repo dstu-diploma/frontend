1216043a0699005012736692cbae4592727fafdd
Nikita Zabashtin <147318691+NikitaKadekomst@users.noreply.github.com>
2025-05-01 21:55:30 +0300

    ✨ Клиентский API для работы с аватаром юзера

--- src/features/user/api/profileApi.ts
+++ src/features/user/api/profileApi.ts
@@ -2,6 +2,7 @@ import axiosInstance from '@/shared/api/axios';
 import Cookies from 'js-cookie';
 import { useMutation } from '@tanstack/react-query';
 import { ProfileFormData } from '../model/schemas';
+import { RefreshResponseBody } from '../model/types';
 
 const API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost';
 
@@ -23,6 +24,31 @@ export const profileApi = {
       reader.readAsDataURL(file);
     });
   },
+  setOrUpdateAvatar: async (file: File): Promise<RefreshResponseBody> => {
+    const formData = new FormData()
+    formData.append('file', file)
+    const response = await axiosInstance.put(`${API_URL}/user/avatar`, formData, {
+      headers: {
+        'Content-Type': "multipart/form-data"
+      }
+    });
+    return response.data;
+  },
+  deleteAvatar: async () => {
+    const response = await axiosInstance.delete(`${API_URL}/user/avatar`)
+    return response.data;
+  },
+  getAvatar: (user_id: number) => {
+      return API_URL + '/uploads/avatars/' + user_id + '.jpg'
+  },
+  isAvatarExists: async (url: string): Promise<boolean> => {
+      return new Promise((resolve) => {
+        const img = new Image();
+        img.onload = () => resolve(true);  
+        img.onerror = () => resolve(false);
+        img.src = url;
+      });
+  }
 }; 
 
 export const profileApiMutations = {
@@ -31,9 +57,14 @@ export const profileApiMutations = {
       mutationFn: profileApi.updateProfile
     });
   },
-  updateAvatar: () => {
+  setOrUpdateAvatar: () => {
     return useMutation({
-      mutationFn: profileApi.updateAvatar
+      mutationFn: profileApi.setOrUpdateAvatar
+    });
+  },
+  deleteAvatar: () => {
+    return useMutation({
+      mutationFn: profileApi.deleteAvatar,
     });
   },
 }
