a0bdd2fba695a185e786a04d2f47b45d9e64dcee
Nikita Zabashtin <147318691+NikitaKadekomst@users.noreply.github.com>
2025-05-01 21:56:36 +0300

    ♻️ 💄 Выделение блока аватара с выпадающим меню в форме профиля в отдельный компонент

---
+++ src/features/user/ui/profile/ProfileAvatar.tsx
@@ -0,0 +1,96 @@
+'use client';
+
+import { Camera, Trash2 } from 'lucide-react';
+import {
+  Avatar,
+  AvatarImage,
+  AvatarFallback,
+} from '@/shared/ui/shadcn/avatar';
+import {
+  DropdownMenu,
+  DropdownMenuTrigger,
+  DropdownMenuContent,
+  DropdownMenuItem,
+} from '@/shared/ui/shadcn/dropdown';
+import { User } from '../../model/types';
+import styles from '../../styles/profileAvatar.module.css';
+import { useProfileAvatar } from '../../hooks/useProfileAvatar';
+
+interface ProfileAvatarProps {
+  profile: User;
+}
+
+export function ProfileAvatar({ profile }: ProfileAvatarProps) {
+  const {
+    menuOpen,
+    setMenuOpen,
+    fileInputRef,
+    avatarSrc, 
+    handleAvatarChange,
+    handleDeleteAvatar
+  } = useProfileAvatar()
+
+  return (
+    <DropdownMenu open={menuOpen} onOpenChange={setMenuOpen}>
+      <DropdownMenuTrigger asChild className={styles.profileAvatarTrigger}>
+        <button
+          className={styles.profileAvatarTriggerButton}
+          onClick={(e) => {
+            e.preventDefault();
+            setMenuOpen((prev) => !prev);
+          }}
+        >
+          <Avatar key={avatarSrc || 'fallback'} className={styles.profileAvatarBlock}>
+            {avatarSrc ? (
+              <AvatarImage
+                src={avatarSrc}
+                alt="User avatar"
+                onError={() => {
+                  handleDeleteAvatar();
+                }}
+              />
+            ) : (
+              <AvatarFallback className={styles.profileAvatarFallback}>
+                <span>{profile.first_name?.[0]?.toUpperCase() || '?'}</span>
+              </AvatarFallback>
+            )}
+          </Avatar>
+        </button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent
+        align="start"
+      >
+        <DropdownMenuItem
+          className={styles.dropdownMenuItem}
+          onSelect={(e) => {
+            e.preventDefault();
+            fileInputRef.current?.click();
+          }}
+        >
+          <Camera />
+          <span>Добавить фото</span>
+          <input
+            ref={fileInputRef}
+            type="file"
+            accept="image/*"
+            onChange={handleAvatarChange}
+            className={styles.dropdownFileInput}
+            style={{ display: 'none' }}
+          />
+        </DropdownMenuItem>
+        {avatarSrc && (
+          <DropdownMenuItem
+            onSelect={(e) => {
+              e.preventDefault();
+              handleDeleteAvatar();
+            }}
+            className={styles.dropdownMenuItem}
+          >
+            <Trash2 />
+            <span>Удалить фото</span>
+          </DropdownMenuItem>
+        )}
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+}
