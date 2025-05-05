f784f4894a8b25e94cc787957ab27200633bcebd
Nikita Zabashtin <147318691+NikitaKadekomst@users.noreply.github.com>
2025-05-05 23:42:36 +0300

    🐛 Доработка интерсептора запросов для корректного обновления токенов

--- src/shared/api/axios.ts
+++ src/shared/api/axios.ts
@@ -1,9 +1,8 @@
 import axios from 'axios';
-import { getAuthCookies, removeAuthCookies, setTokensCookie } from '../lib/helpers/cookies';
+import { cookiesApi } from '../lib/helpers/cookies';
 import { AuthService } from '../lib/services/auth.service';
-import { userApi } from '@/features/user';
 
-const API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost';
+export const API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost';
 
 const axiosInstance = axios.create({
   baseURL: API_URL,
@@ -14,7 +13,7 @@ const axiosInstance = axios.create({
 
 axiosInstance.interceptors.request.use(
   (config) => {
-    const { accessToken } = getAuthCookies();
+    const accessToken = cookiesApi.getAccessToken()
     
     if (accessToken) {
       config.headers.Authorization = `Bearer ${accessToken}`;
@@ -32,32 +31,35 @@ axiosInstance.interceptors.response.use(
   async (error) => {
     const originalRequest = error.config;
     
-    if (error.response?.status === 401 && !originalRequest._retry) {
+    if (error.response?.status === 403 && !originalRequest._retry) {
       originalRequest._retry = true;
       
-      const { refreshToken } = getAuthCookies();
+      const refreshToken = cookiesApi.getRefreshToken()
       
       if (!refreshToken) {
         AuthService.gracefulLogout()
         return Promise.reject(error);
       }
-      
+
       try {
-        const data = await userApi.refreshAccessToken()
+        const response = await axios.post(`${API_URL}/user/access_token`, {}, {
+          headers: {
+            'Authorization': `Bearer ${refreshToken}`
+          }
+        });
         
-        setTokensCookie({
-          accessToken: data.access_token,
+        cookiesApi.setTokensCookie({
+          accessToken: response.data.access_token,
           refreshToken: refreshToken,
         });
         
-        originalRequest.headers.Authorization = `Bearer ${data.access_token}`;
-
+        originalRequest.headers.Authorization = `Bearer ${response.data.access_token}`;
         return axiosInstance(originalRequest);
-      } catch (refreshError) {
-        removeAuthCookies();
+      } catch (error) {
+        cookiesApi.removeAuthCookies();
         localStorage.setItem('login_redirect_reason', 'session_expired');
         window.location.href = '/login';
-        return Promise.reject(refreshError);
+        return Promise.reject(error);
       }
     }
     
