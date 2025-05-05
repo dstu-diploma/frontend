a802e2c4ece30d26dbe75fb89085dec1637ad29d
Nikita Zabashtin <147318691+NikitaKadekomst@users.noreply.github.com>
2025-05-06 00:37:21 +0300

    🏷️ Обновление структуры типов и схем валидации для фичи команды

--- src/features/team/model/types.ts
+++ src/features/team/model/types.ts
@@ -1,38 +1,66 @@
-export interface TeamMember {
-    id: string;
-    firstName: string;
-    lastName: string;
-    avatarUrl?: string;
-    role?: string;
+// Объект информации о команде
+export interface TeamInfo {
+  id: number
+  name: string
+  mates: TeamMateRef[]
 }
 
+// Объект cсылки на участника команды
+export interface TeamMateRef {
+  team_id: number
+  user_id: number
+  is_captain: boolean
+  role_desc: string
+}
+
+// Объект участника команды
 export interface TeamMate {
-    team_id: number
-    user_id: number
-    is_captain: boolean
+  id: number,
+  first_name: string,
+  last_name: string,
+  patronymic: string,
+  register_date: string
+  is_captain: boolean
 }
 
+// Объект приглашения
 export interface TeamInvite {
-    team_id: number
-    user_id: number
+  team_id: number
+  user_id: number
 }
 
+// Объект роли в команде
+export interface TeamRole {
+  role_desc: string
+}
+
+// Объект запроса и ответа для смены капитана
+export interface TeamCaptainRequestBody {
+  user_id: number
+  is_captain: boolean
+}
+export interface TeamCaptainResponseBody {
+  team_id: number
+  user_id: number
+  is_captain: boolean
+  role_desc: string
+}
+
+// Объекта запроса и ответа для создания команды
 export interface TeamCreateRequestBody {
-    name: string
+  name: string
 }
-
 export interface TeamCreateResponseBody {
-    id: number
-    name: string
-    owner_id: number
+  id: number
+  name: string
 }
 
-export interface TeamChangeOwnerRequestBody {
-    user_id: number
+// Объекта запроса и ответа для смены имени команды
+export interface TeamRenameRequestBody {
+    name: string
 }
-
-export interface TeamChangeOwnerResponseBody {
+export interface TeamRenameResponseBody {
     id: number
     name: string
-    owner_id: number
 }
+
