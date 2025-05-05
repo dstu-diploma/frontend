6823c40b46e05288f8c423bec490abb1f9dce587
Nikita Zabashtin <147318691+NikitaKadekomst@users.noreply.github.com>
2025-05-06 00:35:17 +0300

    ✨ 💄 Компонен карточки участника на странице Моя команда

--- src/features/team/ui/cards/TeamMemberCard/TeamMemberCardAvatar/index.tsx
+++ src/features/team/ui/cards/TeamMemberCard/TeamMemberCardAvatar/index.tsx
@@ -1,28 +1,43 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Image from 'next/image'
 import styles from './TeamMemberCardAvatar.module.scss'
 import { UserPartial } from '@/features/user/model/types'
+import { userApi } from '@/features/user'
 
 interface TeamMemberCardAvatarProps {
   member: UserPartial
 }
 
 const TeamMemberCardAvatar = ({ member }: TeamMemberCardAvatarProps) => {
+  const [avatarUrl, setAvatarUrl] = useState('')
+
+  useEffect(() => {
+    const prepareAvatar = async () => {
+      const avatarPath = userApi.getAvatar(member.id)
+      const pathIsValid = await userApi.isAvatarExists(avatarPath)
+
+      if (pathIsValid) {
+        setAvatarUrl(avatarPath)
+      }
+    }
+    prepareAvatar()
+  }, [])
+
   return (
     <div className={styles.avatarContainer}>
-        {member.avatarUrl ? (
-          <Image 
-            src={'/placeholder.png'} 
-            alt={`${member.first_name} ${member.last_name}`}
-            className={styles.avatarImage}
-            width={200}
-            height={200}
-          />
-        ) : (
-          <div className={styles.avatarPlaceholder}>
-            {member.first_name.charAt(0)}{member.last_name.charAt(0)}
-          </div>
-        )}
+      {avatarUrl ? (
+        <Image 
+          src={avatarUrl} 
+          alt={`${member.first_name} ${member.last_name}`}
+          className={styles.avatarImage}
+          width={200}
+          height={200}
+        />
+      ) : (
+        <div className={styles.avatarPlaceholder}>
+          {member.first_name.charAt(0)}{member.last_name.charAt(0)}
+        </div>
+      )}
     </div>
   )
 }
--- src/features/team/ui/cards/TeamMemberCard/index.tsx
+++ src/features/team/ui/cards/TeamMemberCard/index.tsx
@@ -1,19 +1,58 @@
 import styles from './TeamMemberCard.module.scss'
 import { UserPartial } from '@/features/user/model/types';
 import TeamMemberCardAvatar from './TeamMemberCardAvatar';
+import { Button } from '@/shared/ui/shadcn/button';
+import { cookiesApi } from '@/shared/lib/helpers/cookies';
+import { TeamConfirmModal } from '../../modals/TeamConfirmModal';
+import { useTeam } from '@/features/team/hooks/useTeam';
 
 interface TeamMemberCardProps {
   member: UserPartial;
+  isCaptain: boolean;
 }
 
-export const TeamMemberCard = ({ member }: TeamMemberCardProps) => {
+export const TeamMemberCard = ({ member, isCaptain }: TeamMemberCardProps) => {
+  const user = cookiesApi.getUser()
+  const { handleTeamKick, handleChangeCaptain } = useTeam()
+  
   return (
     <div className={styles.card}>
-      <TeamMemberCardAvatar member={member} />
-      <div className={styles.info}>
-        <h4 className={styles.name}>{member.first_name} {member.last_name}</h4>
-        {member.is_captain && <p className={styles.role}>{member.role}</p>}
+      <div className={styles.mateInfo}>
+        <TeamMemberCardAvatar member={member} />
+        <div className={styles.info}>
+          <h4 className={styles.name}>{member.first_name} {member.last_name}</h4>
+          <p className={styles.role}>
+            {member.is_captain && 'Капитан, '}{member?.role_desc ? member?.role_desc: 'Роль в команде не указана'}
+          </p>
+        </div>
       </div>
+      {member.id !== user.id && isCaptain &&
+        <div 
+          className={styles.captainControls}
+          onClick={(e) => {
+            e.stopPropagation();
+            e.preventDefault();
+          }}
+        >
+          <TeamConfirmModal
+            title={`Назначить пользователя ${member.first_name} ${member.last_name} капитаном?`}
+            submitButtonText='Назначить'
+            onConfirm={event => handleChangeCaptain(event, member.id)}
+          >
+            <Button>Установить капитаном</Button>
+          </TeamConfirmModal>
+          <TeamConfirmModal
+            title='Вы действительно хотите исключить данного пользователя?'
+            submitButtonText='Покинуть'
+            onConfirm={event => handleTeamKick(event, member.id)}
+          >
+            <Button 
+              variant='destructive'
+              className={styles.kickButton}
+            >Исключить</Button> 
+          </TeamConfirmModal>
+        </div>
+      }
     </div>
   );
 };
