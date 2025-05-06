ecafa40cee89d04f04b9936aeb2bc3de029164b8
Nikita Zabashtin <147318691+NikitaKadekomst@users.noreply.github.com>
2025-05-06 20:04:45 +0300

    ✨ 🐛 Добавление логики вывода тоста при неудаче прохода через middleware авторизации

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -3,6 +3,9 @@
 import { LoginForm } from '@/features/user/';
 import { useLogin } from '@/features/user/hooks/auth/useLogin';
 import LayoutFallback from '@/shared/ui/custom/LayoutFallback/LayoutFallback';
+import { useToast } from '@/shared/hooks/use-toast';
+import { useEffect } from 'react';
+import Cookies from 'js-cookie';
 
 export default function LoginPage() {
   const {
@@ -11,6 +14,25 @@ export default function LoginPage() {
     error,
     isSubmitting
   } = useLogin()
+  
+  const { toast } = useToast();
+  
+  useEffect(() => {
+    const message = Cookies.get('login_redirect_reason');
+    if (message) {
+      const timer = setTimeout(() => {
+        toast({
+          title: "Ошибка при доступе к маршруту",
+          description: message,
+          variant: "destructive",
+          duration: 5000,
+        });
+        Cookies.remove('login_redirect_reason');
+      }, 100);
+
+      return () => clearTimeout(timer);
+    }
+  }, [toast]);
 
   if (!hasCheckedAuth) {
     return (
