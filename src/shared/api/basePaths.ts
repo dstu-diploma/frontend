e5696543441ec1744ec00b8331e5b53e796ba693
Nikita Zabashtin <147318691+NikitaKadekomst@users.noreply.github.com>
2025-05-07 16:51:11 +0300

    ✨ ♻️ Переработка файла с базовыми путями к клиентскому API сервисов

--- src/shared/api/basePaths.ts
+++ src/shared/api/basePaths.ts
@@ -1,5 +1,18 @@
+// Основной базовый URL для всех API
 export const API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost';
+
+// Базовые URL для всех сервисов
 export const USER_SERVICE_API_URL = API_URL ? API_URL + '/user' : 'http://localhost/user';
 export const TEAM_SERVICE_API_URL = API_URL ? API_URL + '/team' : 'http://localhost/team';
 export const HACKATHON_SERVICE_API_URL = API_URL ? API_URL + '/hackathon' : 'http://localhost/hackathon';
 
+// Базовые URL для админки
+export const USER_SERVICE_ADMIN_API_URL = USER_SERVICE_API_URL + '/admin';
+export const TEAM_SERVICE_ADMIN_API_URL = TEAM_SERVICE_API_URL + '/admin';
+export const HACKATHON_SERVICE_ADMIN_API_URL = HACKATHON_SERVICE_API_URL + '/admin';
+
+// Базовый путь для управления хакатонами
+export const HACKATHON_SERVICE_MANAGE_API_URL = HACKATHON_SERVICE_API_URL + '/manage';
+
+// Базовый путь для управления хакатоновскими командами
+export const TEAM_SERVICE_HACKATHON_TEAM_API_URL = TEAM_SERVICE_API_URL + '/hackathon';
