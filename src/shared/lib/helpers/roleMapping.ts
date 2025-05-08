d4b3329992f3538828ef03b39e566f724ecf7f48
Nikita Zabashtin <147318691+NikitaKadekomst@users.noreply.github.com>
2025-05-08 22:02:36 +0300

    🐛 ✨ Доработка маппинга под требования админ-панели

--- src/shared/lib/helpers/roleMapping.ts
+++ src/shared/lib/helpers/roleMapping.ts
@@ -1,11 +1,17 @@
-const roleMap: Record<string, string> = {
-    'user': 'Участник',
-    'admin': 'Администратор',
-    'organizer': 'Организатор',
-    'helper': 'Техническая поддержка',
-    'jury': 'Член жюри'
-}
+export const roleMap: Record<string, string> = {
+  user: 'Участник',
+  admin: 'Администратор',
+  organizer: 'Организатор',
+  helper: 'Тех.поддержка',
+  jury: 'Член жюри',
+} as const
+
+export type RoleMapValues = (typeof roleMap)[keyof typeof roleMap]
 
 export const mapRole = (roleKey: string): string => {
-    return roleKey in roleMap ? roleMap[roleKey] : roleKey
+  return roleKey in roleMap ? roleMap[roleKey] : roleKey
+}
+
+export const mapRoleKey = (role: string): string => {
+  return Object.keys(roleMap).find(key => roleMap[key] === role) || role
 }
