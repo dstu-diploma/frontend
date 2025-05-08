747231329287099f79ab8865d7a6aaff127e3106
Nikita Zabashtin <147318691+NikitaKadekomst@users.noreply.github.com>
2025-05-08 22:04:30 +0300

    ♻️ Доработка базовых путей для требований системы

--- src/shared/api/basePaths.ts
+++ src/shared/api/basePaths.ts
@@ -1,18 +1,21 @@
 // Основной базовый URL для всех API
-export const API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost';
+export const API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost'
 
 // Базовые URL для всех сервисов
-export const USER_SERVICE_API_URL = API_URL ? API_URL + '/user' : 'http://localhost/user';
-export const TEAM_SERVICE_API_URL = API_URL ? API_URL + '/team' : 'http://localhost/team';
-export const HACKATHON_SERVICE_API_URL = API_URL ? API_URL + '/hackathon' : 'http://localhost/hackathon';
+export const USER_SERVICE_API_URL = API_URL + '/user'
+export const TEAM_SERVICE_API_URL = API_URL + '/team'
+export const HACKATHON_SERVICE_API_URL = API_URL + '/hackathon'
 
 // Базовые URL для админки
-export const USER_SERVICE_ADMIN_API_URL = USER_SERVICE_API_URL + '/admin';
-export const TEAM_SERVICE_ADMIN_API_URL = TEAM_SERVICE_API_URL + '/admin';
-export const HACKATHON_SERVICE_ADMIN_API_URL = HACKATHON_SERVICE_API_URL + '/admin';
+export const USER_SERVICE_ADMIN_API_URL = USER_SERVICE_API_URL + '/admin'
+export const TEAM_SERVICE_ADMIN_API_URL = TEAM_SERVICE_API_URL + '/admin'
+export const HACKATHON_SERVICE_ADMIN_API_URL =
+  HACKATHON_SERVICE_API_URL + '/admin'
 
 // Базовый путь для управления хакатонами
-export const HACKATHON_SERVICE_MANAGE_API_URL = HACKATHON_SERVICE_API_URL + '/manage';
+export const HACKATHON_SERVICE_MANAGE_API_URL =
+  HACKATHON_SERVICE_API_URL + '/manage'
 
 // Базовый путь для управления хакатоновскими командами
-export const TEAM_SERVICE_HACKATHON_TEAM_API_URL = TEAM_SERVICE_API_URL + '/hackathon';
+export const TEAM_SERVICE_HACKATHON_TEAM_API_URL =
+  TEAM_SERVICE_API_URL + '/hackathon'
