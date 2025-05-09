50e7af61aa5c361279975bb776bd6b589745cfee
Nikita Zabashtin <147318691+NikitaKadekomst@users.noreply.github.com>
2025-05-09 20:44:16 +0300

    🎨 Доработка компонента тулбара для удобства стилизации

--- src/shared/ui/custom/Toolbar/Toolbar.tsx
+++ src/shared/ui/custom/Toolbar/Toolbar.tsx
@@ -1,16 +1,14 @@
 import React from 'react'
 import styles from './Toolbar.module.scss'
+import clsx from 'clsx'
 
 interface ToolbarProps {
+  className?: string
   children: React.ReactNode
 }
 
-const Toolbar = ({ children }: ToolbarProps) => {
-  return (
-    <div className={styles.toolbar}>
-      {children}
-    </div>
-  )
+const Toolbar = ({ children, className }: ToolbarProps) => {
+  return <div className={clsx(styles.toolbar, className)}>{children}</div>
 }
 
 export default Toolbar
