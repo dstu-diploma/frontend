e769485b89008a2947fccaff257e0a2a8e4bf8e7
Nikita Zabashtin <147318691+NikitaKadekomst@users.noreply.github.com>
2025-05-11 21:39:16 +0300

    ✨ 💄 Функционал критериев: блок с выводом и действиями

--- src/features/hackatons/hooks/useHackathonCriteria.ts
+++ src/features/hackatons/hooks/useHackathonCriteria.ts
@@ -4,19 +4,38 @@ import { AxiosError } from 'axios'
 import { CriterionFormData } from '../model/schemas'
 import { Criterion } from '../model/types'
 import { useEffect, useState } from 'react'
+import { hackathonBaseApi } from '../api/hackathonBaseApi'
+import { hackathonApi } from '../api'
 
 export const useHackathonCriteria = (page_id: number) => {
+  const { mutate: getCriteriaList } = hackathonApi.getCriteriaList()
   const { mutate: createCriterion } = adminApi.createCriterion()
   const { mutate: updateCriterion } = adminApi.updateCriterion()
-  const { mutate: getCriteriaList } = adminApi.getCriteriaList()
+  const { mutate: deleteCriterion } = adminApi.deleteCriterion()
   const [criteria, setCriteria] = useState<Criterion[]>([])
   const { toast, dismiss } = useToast()
 
-  // Получение текущего списка критериев оценивания работы команды
-  useEffect(() => {
+  // Динамическое обновление списка критериев
+  const loadCriteria = async () => {
     getCriteriaList(page_id, {
       onSuccess: data => setCriteria(data),
+      onError: error => {
+        const axiosError = error as AxiosError
+        if (axiosError.response) {
+          const errorData = axiosError.response.data as { detail: string }
+          toast({
+            variant: 'destructive',
+            title: 'Ошибка при получении списка критериев',
+            description: errorData.detail,
+          })
+        }
+      },
     })
+  }
+
+  // Получение текущего списка критериев оценивания работы команды
+  useEffect(() => {
+    loadCriteria()
   }, [page_id])
 
   // Создание критерия оценивания работы команды
@@ -33,6 +52,7 @@ export const useHackathonCriteria = (page_id: number) => {
           variant: 'defaultBlueSuccess',
           description: `Критерий "${requestBody.name}" успешно создан`,
         })
+        loadCriteria()
       },
       onError: error => {
         dismiss()
@@ -79,6 +99,7 @@ export const useHackathonCriteria = (page_id: number) => {
           variant: 'defaultBlueSuccess',
           description: `Критерий "${requestBody.name}" успешно обновлен`,
         })
+        loadCriteria()
       },
       onError: error => {
         dismiss()
@@ -96,8 +117,59 @@ export const useHackathonCriteria = (page_id: number) => {
     })
   }
 
+  // Удаление критерия оценивания работы команды
+  const handleCriterionDeletion = async (data: CriterionFormData) => {
+    const criterionId = criteria.find(
+      criterion => criterion.name === data.name,
+    )?.id
+
+    if (!criterionId) {
+      toast({
+        variant: 'destructive',
+        title: 'Ошибка при удалении критерия',
+        description: 'Критерий не найден',
+      })
+      return
+    }
+
+    const requestBody = {
+      hackathon_id: Number(page_id),
+      criterion_id: criterionId,
+      name: data.name,
+      weight: Number(data.weight),
+    }
+
+    deleteCriterion(
+      { hackathon_id: Number(page_id), criterion_id: criterionId },
+      {
+        onSuccess: () => {
+          dismiss()
+          toast({
+            variant: 'defaultBlueSuccess',
+            description: `Критерий "${requestBody.name}" успешно удален`,
+          })
+          loadCriteria()
+        },
+        onError: error => {
+          dismiss()
+          const axiosError = error as AxiosError
+          if (axiosError.response) {
+            const errorData = axiosError.response.data as { detail: string }
+            toast({
+              variant: 'destructive',
+              title: 'Ошибка при удалении критерия',
+              description: errorData.detail,
+            })
+          }
+        },
+      },
+    )
+  }
+
   return {
+    criteria,
     handleCriterionCreation,
     handleCriterionUpdate,
+    handleCriterionDeletion,
   }
 }
