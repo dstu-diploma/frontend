2f07f8b525eee283acd08fde0896fffcde412aeb
Nikita Zabashtin <147318691+NikitaKadekomst@users.noreply.github.com>
2025-05-11 21:34:34 +0300

    ✨ Фиксы и добавление различных хелперов в ролевой маппинг

--- src/shared/lib/helpers/roleMapping.ts
+++ src/shared/lib/helpers/roleMapping.ts
@@ -1,9 +1,13 @@
+import { cookiesApi } from './cookies'
+
+const user = cookiesApi.getUser()
+
 export const roleMap: Record<string, string> = {
   user: 'Участник',
   admin: 'Администратор',
   organizer: 'Организатор',
   helper: 'Тех.поддержка',
-  jury: 'Член жюри',
+  judge: 'Член жюри',
 } as const
 
 export type RoleMapValues = (typeof roleMap)[keyof typeof roleMap]
@@ -14,4 +18,10 @@ export const mapRole = (roleKey: string): string => {
 
 export const mapRoleKey = (role: string): string => {
   return Object.keys(roleMap).find(key => roleMap[key] === role) || role
+}
+
+export const isPrivilegedRole = (): boolean => {
+  return (
+    user.role === 'admin' || user.role === 'organizer' || user.role === 'jury'
+  )
 }
