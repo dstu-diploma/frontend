da7dc46b538015ea1e904cdb4c908309c5bf3413
Nikita Zabashtin <147318691+NikitaKadekomst@users.noreply.github.com>
2025-05-11 21:48:48 +0300

    💄 Добавление служебного свойства для заливки иконок во вкладках основного сайдбара

--- src/widgets/Sidebar/configs/navItemsConfig.tsx
+++ src/widgets/Sidebar/configs/navItemsConfig.tsx
@@ -1,34 +1,45 @@
-import { FaHome } from "react-icons/fa";
-import { IoPersonOutline } from "react-icons/io5";
-import { MdEmojiEvents } from "react-icons/md";
-import { RiTeamLine } from "react-icons/ri";
-import { RiAdminLine } from "react-icons/ri";
+import { IoPersonOutline } from 'react-icons/io5'
+import { MdEmojiEvents } from 'react-icons/md'
+import { RiTeamLine } from 'react-icons/ri'
+import { RiAdminLine } from 'react-icons/ri'
+import { LuMailQuestion } from 'react-icons/lu'
 
 export type NavItem = {
   label: string
   href: string
   icon: React.ReactNode
+  isIconFillable?: boolean
 }
-  
+
 export const navigationItems: NavItem[] = [
   {
     label: 'Профиль',
     href: '/profile',
     icon: <IoPersonOutline />,
+    isIconFillable: true,
   },
   {
     label: 'Хакатоны',
     href: '/hackathons',
     icon: <MdEmojiEvents />,
+    isIconFillable: true,
   },
   {
     label: 'Моя команда',
     href: '/team',
-    icon: <RiTeamLine />
+    icon: <RiTeamLine />,
+    isIconFillable: true,
+  },
+  {
+    label: 'Мои обращения',
+    href: '/requests',
+    icon: <LuMailQuestion />,
+    isIconFillable: false,
   },
   {
     label: 'Админ-панель',
     href: '/admin',
-    icon: <RiAdminLine />
-  }
-];
+    icon: <RiAdminLine />,
+    isIconFillable: true,
+  },
+]
