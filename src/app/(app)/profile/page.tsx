2c2a3b2fcab75801a87940598051cd1e5df006b0
Nikita Zabashtin <147318691+NikitaKadekomst@users.noreply.github.com>
2025-05-11 21:30:40 +0300

    🐛 Небольшие фиксы страницы профиля

--- src/app/(app)/profile/page.tsx
+++ src/app/(app)/profile/page.tsx
@@ -1,45 +1,44 @@
-'use client';
+'use client'
 
-import { userApi, ProfileFormData } from '@/features/user';
-import { cookiesApi } from '@/shared/lib/helpers/cookies';
-import { dateStringToISO } from '@/shared/lib/helpers/date';
-import { BaseUserType } from '@/features/user/model/types';
-import { ProfileForm } from '@/features/user/ui/profile/ProfileForm/ProfileForm';
-import styles from './profile.module.css'
+import { userApi, ProfileFormData } from '@/features/user'
+import { cookiesApi } from '@/shared/lib/helpers/cookies'
+import { dateStringToISO } from '@/shared/lib/helpers/date'
+import { BaseUserType } from '@/features/user/model/types'
+import { ProfileForm } from '@/features/user/ui/profile/ProfileForm/ProfileForm'
+import styles from './profile.module.scss'
 
-const ProfilePage = () => {  
-  const { mutate: updateProfile } = userApi.updateProfile();
+const ProfilePage = () => {
+  const { mutate: updateProfile } = userApi.updateProfile()
 
   const handleSubmit = async (data: ProfileFormData) => {
-    updateProfile({
-      first_name: data.first_name,
-      last_name: data.last_name,
-      patronymic: data.patronymic,
-      email: data.email,
-      about: data.about,
-      birthday: dateStringToISO(data.birthday) || '',
-    },
-    {
-      onSuccess: (data: BaseUserType) => {
-        cookiesApi.setUserCookie(data)
+    updateProfile(
+      {
+        first_name: data.first_name,
+        last_name: data.last_name,
+        patronymic: data.patronymic,
+        email: data.email,
+        about: data.about,
+        birthday: dateStringToISO(data.birthday) || '',
       },
-      onError: (error) => {
-        console.error('Ошибка при обновлении профиля:', error);
+      {
+        onSuccess: (data: BaseUserType) => {
+          cookiesApi.setUserCookie(data)
+        },
+        onError: error => {
+          console.error('Ошибка при обновлении профиля:', error)
+        },
       },
-    }
-    );
-  };
+    )
+  }
 
   return (
     <div className={styles.profileContainer}>
       <div className={styles.profileContent}>
         <h1 className={styles.profileTitle}>Профиль</h1>
-        <ProfileForm 
-          onSubmit={handleSubmit}
-        />
+        <ProfileForm onSubmit={handleSubmit} />
       </div>
     </div>
-  );
-}; 
+  )
+}
 
-export default ProfilePage;
+export default ProfilePage
