6c6183488b4777912f1df220a4223b007b13fa47
Nikita Zabashtin <147318691+NikitaKadekomst@users.noreply.github.com>
2025-05-18 16:30:09 +0300

    ✨ 💄 Добавление тоста об успешной регистрации

--- src/features/user/hooks/auth/useRegister.ts
+++ src/features/user/hooks/auth/useRegister.ts
@@ -1,13 +1,15 @@
-import { AxiosError } from "axios";
-import { useRouter } from "next/navigation";
-import { useState } from "react";
-import { userApi } from "../../api";
-import { RegisterFormData } from "../../model/schemas";
+import { AxiosError } from 'axios'
+import { useRouter } from 'next/navigation'
+import { useState } from 'react'
+import { userApi } from '../../api'
+import { RegisterFormData } from '../../model/schemas'
+import { useToast } from '@/shared/hooks/use-toast'
 
 export const useRegister = () => {
-  const router = useRouter();
-  const { mutate: registerUser } = userApi.register();
-  const [error, setError] = useState<string | null>(null);
+  const router = useRouter()
+  const { mutate: registerUser } = userApi.register()
+  const [error, setError] = useState<string | null>(null)
+  const { toast, dismiss } = useToast()
 
   const handleRegister = async (data: RegisterFormData) => {
     registerUser(
@@ -20,26 +22,33 @@ export const useRegister = () => {
       },
       {
         onSuccess: () => {
-          router.push('/login');
+          router.push('/login')
+          dismiss()
+          toast({
+            variant: 'defaultBlueSuccess',
+            title: 'Регистрация прошла успешно',
+            description: 'Вы успешно зарегистрировались',
+          })
         },
         onError: (error: Error) => {
-          const axiosError = error as AxiosError;
+          const axiosError = error as AxiosError
           if (axiosError.response) {
-            const data = axiosError.response.data as { detail?: string };
-            
+            const data = axiosError.response.data as { detail?: string }
+
             if (data.detail?.toLowerCase().includes('уже зарегистрирован')) {
-              setError('Аккаунт с данной почтой уже существует');
+              setError('Аккаунт с данной почтой уже существует')
             } else {
               console.log(data.detail)
-              setError('Ошибка сервера при авторизации');
+              setError('Ошибка сервера при авторизации')
             }
           } else {
-            setError('Ошибка сервера при авторизации!');
+            setError('Ошибка сервера при авторизации!')
           }
-          console.error('Login failed:', error);
+          console.error('Login failed:', error)
         },
-    });
-  };
+      },
+    )
+  }
 
   return {
     handleRegister,
