babe12116910f6ccdf30aec38bd68553c400a92e
Nikita Zabashtin <147318691+NikitaKadekomst@users.noreply.github.com>
2025-05-18 16:16:53 +0300

    🐛 Фикс сохранения данных в стейте в контексте имени пользователя

--- src/providers/UsernameContext.tsx
+++ src/providers/UsernameContext.tsx
@@ -1,39 +1,44 @@
-import { createContext, useContext, useState, ReactNode } from 'react';
-import { cookiesApi } from '@/shared/lib/helpers/cookies';
+'use client'
+
+import { createContext, useContext, useState, ReactNode } from 'react'
+import { cookiesApi } from '@/shared/lib/helpers/cookies'
 
 export interface Username {
-  first_name: string;
-  last_name: string;
+  first_name: string
+  last_name: string
 }
 
 interface UsernameContextType {
-  username: Username | null;
-  setUsername: (user: Username | null) => void;
+  username: Username | null
+  setUsername: (user: Username | null) => void
 }
 
-const UsernameContext = createContext<UsernameContextType | undefined>(undefined);
+const UsernameContext = createContext<UsernameContextType | undefined>(
+  undefined,
+)
 
 export const UsernameProvider = ({ children }: { children: ReactNode }) => {
   const [username, setUsername] = useState<Username | null>(() => {
     const user = cookiesApi.getUser()
-    return user 
-      ? { 
-        first_name: user.first_name, 
-        last_name:user.last_name 
-      } : null;
-  });
+    return user
+      ? {
+          first_name: user.first_name,
+          last_name: user.last_name,
+        }
+      : null
+  })
 
   return (
     <UsernameContext.Provider value={{ username, setUsername }}>
       {children}
     </UsernameContext.Provider>
-  );
-};
+  )
+}
 
 export const useUsername = () => {
-  const context = useContext(UsernameContext);
+  const context = useContext(UsernameContext)
   if (!context) {
-    throw new Error('useUsername must be used within a UsernameProvider');
+    throw new Error('useUsername must be used within a UsernameProvider')
   }
-  return context;
-};
+  return context
+}
