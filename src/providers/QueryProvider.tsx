0b2af28d65d8e0df28e27b7c5068f542d0f5fd73
Nikita Zabashtin <147318691+NikitaKadekomst@users.noreply.github.com>
2025-05-23 16:06:02 +0300

    🥅 🔧 Обновление конфига React Query для обработки ошибки 400 и настройка ретраев

--- src/providers/QueryProvider.tsx
+++ src/providers/QueryProvider.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { AxiosError } from 'axios'
 import { ReactNode, useState } from 'react'
 
 interface QueryProviderProps {
@@ -8,7 +9,32 @@ interface QueryProviderProps {
 }
 
 export const QueryProvider = ({ children }: QueryProviderProps) => {
-  const [queryClient] = useState(() => new QueryClient())
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            refetchOnWindowFocus: false,
+            retry: (failureCount, error) => {
+              const axiosError = error as AxiosError
+              return axiosError.response?.status !== 400 && failureCount < 2
+            },
+          },
+          mutations: {
+            onError: error => {
+              const axiosError = error as AxiosError
+              if (axiosError.response?.status === 400) {
+                const errorData = axiosError.response.data as {
+                  detail?: string
+                }
+                console.warn(errorData.detail)
+              }
+              throw error
+            },
+          },
+        },
+      }),
+  )
 
   return (
     <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
