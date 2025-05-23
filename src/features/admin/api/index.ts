c01b8f6be753c5980e62e7c5c493f4bb5d3b4a90
Nikita Zabashtin <147318691+NikitaKadekomst@users.noreply.github.com>
2025-05-23 18:26:49 +0300

    🎨 Cбор API админки в индексном файле

--- src/features/admin/api/index.ts
+++ src/features/admin/api/index.ts
@@ -1,9 +1,7 @@
-import { adminHackathonsApi } from './hackathons'
 import { adminTeamsApi } from './teams'
 import { adminUsersApi } from './users'
 
 export const adminApi = {
   ...adminUsersApi,
   ...adminTeamsApi,
-  ...adminHackathonsApi,
 }
