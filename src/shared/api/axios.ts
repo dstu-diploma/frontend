7f85494601eb44ecbab711f04714a8d5358a0075
Nikita Zabashtin <147318691+NikitaKadekomst@users.noreply.github.com>
2025-05-23 16:05:11 +0300

    🥅 Добавление в интерсептор обработки 400 ошибки

--- src/shared/api/axios.ts
+++ src/shared/api/axios.ts
@@ -1,68 +1,81 @@
-import axios from 'axios';
-import { cookiesApi } from '../lib/helpers/cookies';
-import { AuthService } from '../lib/services/auth.service';
+import axios from 'axios'
+import { cookiesApi } from '../lib/helpers/cookies'
+import { AuthService } from '../lib/services/auth.service'
 
-export const API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost';
+export const API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost'
 
 const axiosInstance = axios.create({
   baseURL: API_URL,
   headers: {
     'Content-Type': 'application/json',
   },
-});
+})
 
 axiosInstance.interceptors.request.use(
-  (config) => {
+  config => {
     const accessToken = cookiesApi.getAccessToken()
-    
+
     if (accessToken) {
-      config.headers.Authorization = `Bearer ${accessToken}`;
+      config.headers.Authorization = `Bearer ${accessToken}`
     }
-    
-    return config;
+
+    return config
   },
-  (error) => {
-    return Promise.reject(error);
-  }
-);
+  error => {
+    return Promise.reject(error)
+  },
+)
 
 axiosInstance.interceptors.response.use(
-  (response) => response,
-  async (error) => {
-    const originalRequest = error.config;
-    
+  response => response,
+  async error => {
+    const originalRequest = error.config
+
+    if (error.response?.status === 400) {
+      return Promise.reject({
+        message: error.response.data?.detail || 'Bad Request',
+        status: 400,
+        isAxiosError: true,
+        response: error.response,
+      })
+    }
+
     if (error.response?.status === 403 && !originalRequest._retry) {
-      originalRequest._retry = true;
-      
+      originalRequest._retry = true
+
       const refreshToken = cookiesApi.getRefreshToken()
-      
+
       if (!refreshToken) {
         AuthService.gracefulLogout()
-        return Promise.reject(error);
+        return Promise.reject(error)
       }
 
       try {
-        const response = await axios.post(`${API_URL}/user/access_token`, {}, {
-          headers: {
-            'Authorization': `Bearer ${refreshToken}`
-          }
-        });
-        
+        const response = await axios.post(
+          `${API_URL}/user/access_token`,
+          {},
+          {
+            headers: {
+              Authorization: `Bearer ${refreshToken}`,
+            },
+          },
+        )
+
         cookiesApi.setTokensCookie({
           accessToken: response.data.access_token,
           refreshToken: refreshToken,
-        });
-        
-        originalRequest.headers.Authorization = `Bearer ${response.data.access_token}`;
-        return axiosInstance(originalRequest);
+        })
+
+        originalRequest.headers.Authorization = `Bearer ${response.data.access_token}`
+        return axiosInstance(originalRequest)
       } catch (error) {
-        AuthService.gracefulLogout();
-        return Promise.reject(error);
+        AuthService.gracefulLogout()
+        return Promise.reject(error)
       }
     }
-    
-    return Promise.reject(error);
-  }
-);
 
-export default axiosInstance; 
+    return Promise.reject(error)
+  },
+)
+
+export default axiosInstance
