d3905ae451599fea674c2f8338d14d38499486aa
Nikita Zabashtin <147318691+NikitaKadekomst@users.noreply.github.com>
2025-05-23 18:28:32 +0300

    ✨ Добавление в контексте учета статуса удаления аватарки

--- src/features/user/context/AvatarContext.tsx
+++ src/features/user/context/AvatarContext.tsx
@@ -1,22 +1,31 @@
 'use client'
 
-import { createContext, useContext, useState, ReactNode } from 'react'
-import { useInitAvatar } from '../hooks/profile/useInitAvatar'
+import {
+  createContext,
+  useContext,
+  useState,
+  ReactNode,
+  SetStateAction,
+  Dispatch,
+} from 'react'
 
 interface AvatarContextType {
   avatarSrc: string | null
   setAvatarSrc: (src: string | null) => void
+  isAvatarDeleted: boolean
+  setAvatarDeleted: Dispatch<SetStateAction<boolean>>
 }
 
 const AvatarContext = createContext<AvatarContextType | undefined>(undefined)
 
 export function AvatarProvider({ children }: { children: ReactNode }) {
   const [avatarSrc, setAvatarSrc] = useState<string | null>(null)
-
-  useInitAvatar(setAvatarSrc)
+  const [isAvatarDeleted, setAvatarDeleted] = useState<boolean>(false)
 
   return (
-    <AvatarContext.Provider value={{ avatarSrc, setAvatarSrc }}>
+    <AvatarContext.Provider
+      value={{ avatarSrc, setAvatarSrc, isAvatarDeleted, setAvatarDeleted }}
+    >
       {children}
     </AvatarContext.Provider>
   )
