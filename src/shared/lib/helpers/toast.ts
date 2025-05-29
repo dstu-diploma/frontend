84207581f218042e1d4f4d78ba6a29ffa5975fb5
Nikita Zabashtin <147318691+NikitaKadekomst@users.noreply.github.com>
2025-05-29 19:33:01 +0300

    ✨ Единый сервис клиентских уведомлений на базе тостов

--- src/shared/lib/helpers/toast.ts
+++ src/shared/lib/helpers/toast.ts
@@ -1,47 +1,60 @@
-"use client"
+'use client'
 
 import { useToast } from '@/shared/hooks/use-toast'
 import { AxiosError } from 'axios'
+import { useCallback } from 'react'
 
 export const useCustomToast = () => {
   const { toast, dismiss } = useToast()
 
-  const showToastError = (error: unknown, errorMessage: string) => {
-    const axiosError = error as AxiosError
-    if (axiosError.response) {
-      const errorData = axiosError.response.data as { detail?: string }
-      if (
-        errorData.detail ===
-        'Данный пользователь не является членом какой-либо команды!'
-      ) {
-        return
+  // Вывод тоста с ошибкой типа AxiosError
+  const showToastError = useCallback(
+    (error: unknown, errorMessage: string) => {
+      const axiosError = error as AxiosError
+      if (axiosError.response) {
+        const errorData = axiosError.response.data as { detail?: string }
+        if (
+          errorData.detail ===
+          'Данный пользователь не является членом какой-либо команды!'
+        ) {
+          return
+        }
+        dismiss()
+        toast({
+          title: errorMessage,
+          variant: 'destructive',
+          description: errorData.detail,
+        })
       }
+      console.warn(errorMessage, error)
+    },
+    [dismiss, toast],
+  )
+
+  // Вывод тоста с текстовой ошибкой
+  const showRawToastError = useCallback(
+    (title: string, description: string) => {
       dismiss()
       toast({
-        title: errorMessage,
         variant: 'destructive',
-        description: errorData.detail,
+        title: title,
+        description: description,
       })
-    }
-    console.warn(errorMessage, error)
-  }
+    },
+    [dismiss, toast],
+  )
 
-  const showRawToastError = (title: string, description: string) => {
-    dismiss()
-    toast({
-      variant: 'destructive',
-      title: title,
-      description: description,
-    })
-  }
-
-  const showToastSuccess = (successMessage: string) => {
-    dismiss()
-    toast({
-      variant: 'defaultBlueSuccess',
-      description: successMessage,
-    })
-  }
+  // Вывод успешного тоста
+  const showToastSuccess = useCallback(
+    (successMessage: string) => {
+      dismiss()
+      toast({
+        variant: 'defaultBlueSuccess',
+        description: successMessage,
+      })
+    },
+    [dismiss, toast],
+  )
 
   return {
     showToastError,
