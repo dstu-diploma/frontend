27505f247e56b7fb7a32396353e4b72032c9d200
Nikita Zabashtin <147318691+NikitaKadekomst@users.noreply.github.com>
2025-05-29 19:29:10 +0300

    💥 Обращения: хуки с обработчиками действий и подпиской на сокеты

--- src/features/requests/hooks/useRequests.ts
+++ src/features/requests/hooks/useRequests.ts
@@ -1,180 +1,58 @@
-import { useToast } from '@/shared/hooks/use-toast'
-import { AxiosError } from 'axios'
+import { notificationService } from '@/shared/lib/services/notification.service'
 import { requestsApi } from '../api'
 import { CreateRequestFormData } from '../model/schemas'
-import { useEffect, useState, useCallback, useRef } from 'react'
-import { Request } from '../model/types'
+import { useEffect, useCallback, useMemo } from 'react'
 
 export const useRequests = () => {
-  const { mutate: getAllRequests, isPending: isLoadingRequests } =
-    requestsApi.getAllRequests()
-  const { mutate: createRequest } = requestsApi.createRequest()
-  const { mutate: closeRequest } = requestsApi.closeRequest()
-  const { mutate: getRequestById } = requestsApi.getRequestById()
+  const {
+    data: allRequests,
+    isSuccess: isAllRequestsLoaded,
+    isError: isRequestsLoadError,
+    isLoading: isRequestsLoading,
+    error: requestsLoadError,
+  } = requestsApi.useGetAllRequests()
+  const { mutate: createRequest } = requestsApi.useCreateRequest()
 
-  const [requests, setRequests] = useState<Request[]>([])
-  const [currentRequest, setCurrentRequest] = useState<Request | null>(null)
-  const [isCurrentRequestLoaded, setIsCurrentRequestLoaded] = useState(false)
-  const [isAllRequestsLoaded, setIsAllRequestsLoaded] = useState(false)
-  const [loadingRequestIds, setLoadingRequestIds] = useState<Set<number>>(
-    new Set(),
-  )
-  const initialLoadDone = useRef(false)
-  const { toast, dismiss } = useToast()
+  // Типа обращений
+  const closedRequests = useMemo(() => {
+    return allRequests?.filter(request => request.closed_by_user_id)
+  }, [allRequests])
+  const openedRequests = useMemo(() => {
+    return allRequests?.filter(request => !request.closed_by_user_id)
+  }, [allRequests])
 
   // Функция для загрузки всех обращений
-  const loadRequests = useCallback(async () => {
-    setIsAllRequestsLoaded(false)
-    getAllRequests(undefined, {
-      onSuccess: data => {
-        setRequests(data)
-        initialLoadDone.current = true
-        setIsAllRequestsLoaded(true)
-      },
-      onError: error => {
-        dismiss()
-        const axiosError = error as AxiosError
-        if (axiosError.response) {
-          const errorData = axiosError.response.data as { detail: string }
-          toast({
-            variant: 'destructive',
-            title: 'Ошибка при загрузки обращений',
-            description: errorData.detail,
-          })
-          console.error('Ошибка при загрузке обращений:', errorData)
-        }
-        setIsAllRequestsLoaded(false)
-      },
-    })
-  }, [getAllRequests, dismiss, toast])
-
-  // Функция для загрузки обращения по id
-  const loadRequestById = useCallback(
-    async (requestId: number) => {
-      setLoadingRequestIds(prev => new Set(prev).add(requestId))
-      getRequestById(requestId, {
-        onSuccess: data => {
-          setCurrentRequest(data)
-          setIsCurrentRequestLoaded(true)
-          setLoadingRequestIds(prev => {
-            const newSet = new Set(prev)
-            newSet.delete(requestId)
-            return newSet
-          })
-        },
-        onError: error => {
-          console.error('Error loading request:', requestId, error)
-          dismiss()
-          const axiosError = error as AxiosError
-          if (axiosError.response) {
-            const errorData = axiosError.response.data as { detail: string }
-            toast({
-              variant: 'destructive',
-              title: 'Ошибка при получении данных о текущем обращении',
-              description: errorData.detail,
-            })
-            console.error(
-              'Ошибка при получении данных о текущем обращении:',
-              errorData,
-            )
-          }
-          setIsCurrentRequestLoaded(false)
-          setLoadingRequestIds(prev => {
-            const newSet = new Set(prev)
-            newSet.delete(requestId)
-            return newSet
-          })
-        },
-      })
-    },
-    [getRequestById, dismiss, toast],
-  )
-
   useEffect(() => {
-    loadRequests()
-  }, [])
+    if (isRequestsLoadError) {
+      notificationService.error(
+        requestsLoadError,
+        'Ошибка при загрузке обращений',
+      )
+    }
+  }, [allRequests, isRequestsLoadError, requestsLoadError])
 
   // Создание обращения
   const handleCreateRequest = useCallback(
     (data: CreateRequestFormData) => {
       const requestBody = {
+        hackathon_id: Number(data.hackathon_id),
         message: data.message,
         subject: data.subject,
       }
       createRequest(requestBody, {
-        onSuccess: () => {
-          dismiss()
-          toast({
-            variant: 'defaultBlueSuccess',
-            description: `Обращение успешно создано`,
-          })
-          loadRequests()
-        },
         onError: error => {
-          dismiss()
-          const axiosError = error as AxiosError
-          if (axiosError.response) {
-            const errorData = axiosError.response.data as { detail: string }
-            toast({
-              variant: 'destructive',
-              title: 'Ошибка при создании обращения',
-              description: errorData.detail,
-            })
-            console.error('Ошибка при создании обращения:', errorData)
-          }
+          notificationService.error(error, 'Ошибка при создании обращения')
         },
       })
     },
-    [createRequest, dismiss, toast, loadRequests],
-  )
-
-  // Закрытие обращения
-  const handleCloseRequest = useCallback(
-    (e: React.MouseEvent<HTMLButtonElement>, requestId: number) => {
-      e.preventDefault()
-      closeRequest(requestId, {
-        onSuccess: () => {
-          dismiss()
-          toast({
-            variant: 'defaultBlueSuccess',
-            description: `Обращение успешно закрыто`,
-          })
-          loadRequests()
-        },
-        onError: error => {
-          dismiss()
-          const axiosError = error as AxiosError
-          if (axiosError.response?.status === 400) {
-            toast({
-              variant: 'destructive',
-              description: 'Обращение уже закрыто',
-            })
-            return
-          }
-          if (axiosError.response) {
-            const errorData = axiosError.response.data as { detail: string }
-            toast({
-              variant: 'destructive',
-              title: 'Ошибка при закрытии обращения',
-              description: errorData.detail,
-            })
-            console.error('Ошибка при закрытии обращения:', errorData)
-          }
-        },
-      })
-    },
-    [closeRequest, dismiss, toast, loadRequests],
+    [createRequest],
   )
 
   return {
-    requests,
-    isLoadingRequests,
+    closedRequests,
+    openedRequests,
+    isRequestsLoading,
     isAllRequestsLoaded,
-    currentRequest,
-    isCurrentRequestLoaded,
-    loadingRequestIds,
-    loadRequestById,
     handleCreateRequest,
-    handleCloseRequest,
   }
 }
