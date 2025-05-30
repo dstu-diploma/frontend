851f1fbef220d6a3715a9cfdaa25e02c2516cf02
Nikita Zabashtin <147318691+NikitaKadekomst@users.noreply.github.com>
2025-05-30 03:29:59 +0300

    💄 Специфичный UI для страницы хакатоновской команды
    (нет времени делать единый UI для обоих типов команд)

---
+++ src/features/team/ui/hackathonPage/HackathonTeamMembersList/HackathonTeamMembersList.tsx
@@ -0,0 +1,87 @@
+'use client'
+
+import EntityLoading from '@/shared/ui/custom/fallback/EntityLoading'
+import Link from 'next/link'
+import { memo } from 'react'
+import { TeamMemberCard } from '../../cards/TeamMemberCard'
+import styles from './HackathonTeamMembersList.module.scss'
+import { TeamMateRef } from '@/features/team/model/types'
+import { UserPartial } from '@/features/user/model/types'
+
+interface HackathonTeamMembersListProps {
+  user: UserPartial
+  settings: {
+    isTeamMatesLoading: boolean
+    teamMates: TeamMateRef[]
+    isCaptain: boolean
+    handleChangeCaptain: (
+      event: React.FormEvent,
+      member: TeamMateRef,
+    ) => Promise<void>
+    handleTeamKick: (
+      event: React.FormEvent,
+      member: TeamMateRef,
+    ) => Promise<void>
+  }
+}
+
+const HackathonTeamMembersList = memo(
+  ({ user, settings }: HackathonTeamMembersListProps) => {
+    const {
+      isTeamMatesLoading,
+      teamMates,
+      isCaptain,
+      handleChangeCaptain,
+      handleTeamKick,
+    } = settings
+
+    if (isTeamMatesLoading) {
+      return (
+        <div className={styles.teamMembers}>
+          <div className={styles.members}>
+            <EntityLoading />
+          </div>
+        </div>
+      )
+    }
+
+    if (!teamMates?.length) {
+      return (
+        <div className={styles.teamMembers}>
+          <div className={styles.members}>
+            <span className={styles.noMates}>
+              В команде нет активных участников
+            </span>
+          </div>
+        </div>
+      )
+    }
+
+    return (
+      <div className={styles.teamMembers}>
+        {teamMates.map(member => (
+          <Link
+            key={member.user_id}
+            href={
+              member.user_id === user.id
+                ? '/profile'
+                : `/user/${member.user_id}`
+            }
+            className={styles.link}
+          >
+            <TeamMemberCard
+              member={member}
+              isCaptain={isCaptain}
+              onChangeRights={handleChangeCaptain}
+              onKick={handleTeamKick}
+            />
+          </Link>
+        ))}
+      </div>
+    )
+  },
+)
+
+HackathonTeamMembersList.displayName = 'HackathonTeamMembersList'
+
+export default HackathonTeamMembersList
---
+++ src/features/team/ui/hackathonPage/HackathonTeamToolbar/HackathonTeamToolbar.tsx
@@ -0,0 +1,123 @@
+'use client'
+
+import React from 'react'
+import { TeamMateRef } from '@/features/team/model/types'
+import { ActionModal } from '@/shared/ui/custom/modals/ActionModal'
+import { ConfirmModal } from '@/shared/ui/custom/modals/ConfirmModal'
+import Toolbar from '@/shared/ui/custom/misc/Toolbar/Toolbar'
+import { Button } from '@/shared/ui/shadcn/button'
+import TeamSingleFieldFormContent from '../../modals/TeamSingleFieldFormContent'
+import { confirmModalWarning } from '@/features/team/configs/confirmModalWarnings'
+import styles from './HackathonTeamToolbar.module.scss'
+import { UserPartial } from '@/features/user/model/types'
+import { useHackathonSetRoleModal } from '@/features/team/hooks/hackathonTeam/dialogs/useHackathonSetRoleModal'
+import { useAddMemberModal } from '@/features/team/hooks/hackathonTeam/dialogs/useAddMemberModal'
+
+interface HackathonTeamToolbarProps {
+  hackathonId: number
+  user: UserPartial
+  settings: {
+    hasTeam: boolean
+    isCaptain: boolean
+    isTeamLoading: boolean
+    teamName: string
+    teamMates: TeamMateRef[]
+    handleTeamLeave: (event: React.FormEvent) => Promise<void>
+    handleChangeCaptain: (
+      event: React.FormEvent,
+      member: TeamMateRef,
+    ) => Promise<void>
+  }
+}
+
+const HackathonTeamToolbar = (props: HackathonTeamToolbarProps) => {
+  const user = props.user
+  const {
+    hasTeam,
+    isCaptain,
+    isTeamLoading,
+    teamMates,
+    handleTeamLeave,
+    handleChangeCaptain,
+  } = props.settings
+
+  const { role, handleMateRoleChange, handleMateRoleSave } =
+    useHackathonSetRoleModal(props.hackathonId)
+
+  const { mateEmail, handleTeamInviteChange, handleHackathonTeamAdd } =
+    useAddMemberModal(props.hackathonId)
+
+  const modalWarning = confirmModalWarning(teamMates, user)
+
+  return (
+    <Toolbar>
+      <div className={styles.toolbarContent}>
+        {isTeamLoading && <span>Загрузка данных о команде...</span>}
+        {!isTeamLoading && !hasTeam && <span>Вы не состоите в команде</span>}
+        <div className={styles.toolbarControls}>
+          {hasTeam && (
+            <ActionModal
+              title='Установить или обновить роль'
+              submitButtonText='Установить роль'
+              onSave={handleMateRoleSave}
+              trigger={<Button>Установить название роли в команде</Button>}
+            >
+              <TeamSingleFieldFormContent
+                fieldName='role'
+                value={role}
+                onChange={handleMateRoleChange}
+                placeholder='Введите Вашу текущую роль в команде'
+                fieldType='text'
+              />
+            </ActionModal>
+          )}
+          {hasTeam && isCaptain && (
+            <ActionModal
+              title='Добавить участника'
+              submitButtonText='Добавление участника'
+              onSave={handleHackathonTeamAdd}
+              trigger={<Button>Добавить участника</Button>}
+            >
+              <TeamSingleFieldFormContent
+                fieldName='email'
+                fieldType='email'
+                value={mateEmail}
+                onChange={handleTeamInviteChange}
+                placeholder='Введите электронную почту пользователя'
+              />
+            </ActionModal>
+          )}
+          {hasTeam && isCaptain && teamMates && teamMates?.length > 1 && (
+            <ConfirmModal
+              title='Вы действительно хотите снять с себя права капитана?'
+              submitButtonText='Снять права'
+              warning={modalWarning}
+              onConfirm={e =>
+                handleChangeCaptain(
+                  e,
+                  teamMates?.find(
+                    mate => mate.is_captain && mate.user_id == user.id,
+                  ) as TeamMateRef,
+                )
+              }
+            >
+              <Button variant='destructive'>Cнять с себя права капитана</Button>
+            </ConfirmModal>
+          )}
+          {hasTeam && (
+            <ConfirmModal
+              title='Вы действительно хотите покинуть команду?'
+              submitButtonText='Покинуть'
+              warning={modalWarning}
+              onConfirm={handleTeamLeave}
+            >
+              <Button variant='destructive'>Покинуть команду</Button>
+            </ConfirmModal>
+          )}
+        </div>
+      </div>
+    </Toolbar>
+  )
+}
+
+export default HackathonTeamToolbar
