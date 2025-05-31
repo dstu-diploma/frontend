7b39d186aae65f2978a2e4de7104aaa744441908
Nikita Zabashtin <147318691+NikitaKadekomst@users.noreply.github.com>
2025-05-31 14:48:53 +0300

    💄 Адаптация хедера под мобилки

--- src/widgets/Header/ui/Header.tsx
+++ src/widgets/Header/ui/Header.tsx
@@ -3,23 +3,43 @@
 import Link from 'next/link'
 import { Username, useUsername } from '@/providers/UsernameProvider'
 import UserAvatar from './UserAvatar/UserAvatar'
-import styles from './Header.module.css'
+import styles from './Header.module.scss'
 import { useState, useEffect } from 'react'
+import { useScreenSize } from '@/providers/ScreenSizeProvider'
+import clsx from 'clsx'
+import { RxHamburgerMenu } from 'react-icons/rx'
+import MobileSidebar from '@/widgets/Sidebar/ui/MobileSidebar'
+import MediaQuery from 'react-responsive'
 
 export const Header = () => {
   const { username: contextUsername } = useUsername()
   const [username, setUsername] = useState<Username | null>(null)
+
+  const { isMobile, isTablet, isDesktop, isMediumDesktop } = useScreenSize()
+  const headerStyles = clsx(styles.header, {
+    [styles.mobile]: isMobile,
+    [styles.tablet]: isTablet,
+    [styles.desktop]: isDesktop,
+    [styles.mediumDesktop]: isMediumDesktop,
+  })
 
   useEffect(() => {
     setUsername(contextUsername)
   }, [contextUsername])
 
   return (
-    <header className={styles.header}>
+    <header className={headerStyles}>
       <div className={styles.headerContent}>
-        <Link href='/' className={styles.brand}>
-          <h1 className={styles.brandTitle}>Packathon</h1>
-        </Link>
+        <div className={styles.logoBlock}>
+          <MediaQuery maxWidth={1024}>
+            <MobileSidebar>
+              <RxHamburgerMenu className={styles.burgerIcon} />
+            </MobileSidebar>
+          </MediaQuery>
+          <Link href='/' className={styles.brand}>
+            <h1 className={styles.brandTitle}>Packathon</h1>
+          </Link>
+        </div>
         <div className={styles.userInfo}>
           <Link href='/profile' className={styles.userName}>
             <span>
