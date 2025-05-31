4eb87d5323311919574e6d52ede1bb438db89258
Nikita Zabashtin <147318691+NikitaKadekomst@users.noreply.github.com>
2025-05-31 14:49:18 +0300

    💄 Адаптация сайдбара под мобилки в формате отдельной мобильной модалки

--- src/widgets/Sidebar/ui/MobileSidebar.tsx
+++ src/widgets/Sidebar/ui/MobileSidebar.tsx
@@ -1,51 +1,53 @@
 'use client'
 
 import React from 'react'
-import { SheetTrigger, SheetContent } from '@/shared/ui/shadcn/sheet'
-import { Link, Sheet } from 'lucide-react'
+import { SheetTrigger, SheetContent, Sheet } from '@/shared/ui/shadcn/sheet'
 import { useSidebar } from '../hooks/useSidebar'
-import styles from './Sidebar.module.css'
+import styles from './Sidebar.module.scss'
 import clsx from 'clsx'
+import Link from 'next/link'
 
 interface MobileSidebarProps {
-  trigger: React.ReactNode
+  children: React.ReactNode
 }
 
-const MobileSidebar = ({ trigger }: MobileSidebarProps) => {
+const MobileSidebar = ({ children }: MobileSidebarProps) => {
   const { pathname, handleLogout, processedNavigationItems } = useSidebar()
   return (
     <Sheet>
-      <SheetTrigger asChild>{trigger}</SheetTrigger>
-      <SheetContent>
-        <nav className={styles.navigation}>
-          <ul className={styles.navList}>
-            {processedNavigationItems.map(item => (
-              <li key={item.href} className={styles.navItem}>
-                <Link
-                  href={item.href}
-                  className={`${styles.navLink} ${
-                    pathname === item.href ? styles.active : ''
-                  }`}
-                >
-                  <span
-                    className={clsx(
-                      styles.icon,
-                      item.isIconFillable && styles.iconFill,
-                    )}
+      <SheetTrigger asChild>{children}</SheetTrigger>
+      <SheetContent side='left' className={styles.mobileSidebar}>
+        <div className={styles.sidebar}>
+          <nav className={styles.navigation}>
+            <ul className={styles.navList}>
+              {processedNavigationItems.map(item => (
+                <li key={item.href} className={styles.navItem}>
+                  <Link
+                    href={item.href}
+                    className={`${styles.navLink} ${
+                      pathname === item.href ? styles.active : ''
+                    }`}
                   >
-                    {item.icon}
-                  </span>
-                  <span className={styles.label}>{item.label}</span>
-                </Link>
-              </li>
-            ))}
-          </ul>
-        </nav>
-        <footer className={styles.sidebarFooter}>
-          <button onClick={handleLogout} className={styles.logoutButton}>
-            <span className={styles.label}>Выйти</span>
-          </button>
-        </footer>
+                    <span
+                      className={clsx(
+                        styles.icon,
+                        item.isIconFillable && styles.iconFill,
+                      )}
+                    >
+                      {item.icon}
+                    </span>
+                    <span className={styles.label}>{item.label}</span>
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </nav>
+          <footer className={styles.sidebarFooter}>
+            <button onClick={handleLogout} className={styles.logoutButton}>
+              <span className={styles.label}>Выйти</span>
+            </button>
+          </footer>
+        </div>
       </SheetContent>
     </Sheet>
   )
