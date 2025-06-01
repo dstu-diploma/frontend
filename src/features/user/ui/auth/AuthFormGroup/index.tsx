d165c328ba542011c2b3a6fd3838591e43e594ff
Nikita Zabashtin <147318691+NikitaKadekomst@users.noreply.github.com>
2025-06-01 14:25:20 +0300

    💄 Адаптация страниц авторизации под мобилки

--- src/features/user/ui/auth/AuthFormGroup/index.tsx
+++ src/features/user/ui/auth/AuthFormGroup/index.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 import { FieldErrors, UseFormRegister } from 'react-hook-form'
 import styles from './AuthFormGroup.module.scss'
 import { RegisterFormData } from '@/features/user/model/schemas'
+import { useScreenSize } from '@/providers/ScreenSizeProvider'
+import clsx from 'clsx'
 
 interface AuthFormGroupProps {
   label: string
@@ -20,8 +22,16 @@ const AuthFormGroup = ({
   register,
   errors,
 }: AuthFormGroupProps) => {
+  const { isMobile, isTablet, isDesktop, isMediumDesktop } = useScreenSize()
+  const formGroupStyles = clsx(styles.formGroup, {
+    [styles.mobile]: isMobile,
+    [styles.tablet]: isTablet,
+    [styles.desktop]: isDesktop,
+    [styles.mediumDesktop]: isMediumDesktop,
+  })
+
   return (
-    <div className={styles.formGroup}>
+    <div className={formGroupStyles}>
       <label htmlFor={fieldKey}>{label}</label>
       <input
         type={fieldType || 'text'}
